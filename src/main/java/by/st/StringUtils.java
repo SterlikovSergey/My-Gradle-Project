0a703c243a1755c7695958c5bdc0c1fec9cf9332
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-08-20 19:33:30 +0300

    add StringUtils entity and StringUtilsTest entity

---
+++ src/main/java/by/st/StringUtils.java
@@ -0,0 +1,11 @@
+package by.st;
+
+public class StringUtils {
+    public static boolean isPositiveNumber(String str) {
+        try {
+            return Integer.parseInt(str) > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/StringUtilsTest.java
@@ -0,0 +1,14 @@
+import by.st.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringUtilsTest {
+    @Test
+    public void testIsPositiveNumber() {
+        assertTrue(StringUtils.isPositiveNumber("123"));
+        assertFalse(StringUtils.isPositiveNumber("-123"));
+        assertFalse(StringUtils.isPositiveNumber("abc"));
+    }
+}
